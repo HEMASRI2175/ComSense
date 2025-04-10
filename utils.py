6d6bb71a3124f889de04ccfbb301df8459384cbc
CHETTIM CHETTY HEMASRI <131608043+HEMASRI2175@users.noreply.github.com>
2025-04-10 18:47:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,27 +3,31 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_text_splitters import TokenTextSplitter
 from langchain.chains.summarize import load_summarize_chain
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 gemini_api_key = st.secrets['GEMINI_API_KEY']
 
 def get_summary(text):
+    # Tokenization
     text_splitter = TokenTextSplitter(
         chunk_size=1000, 
         chunk_overlap=10
     )
     chunks = text_splitter.create_documents([text])
 
+    # LLM model
     llm = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
         api_key=gemini_api_key
     )
     
+    # Summarization chain
     chain = load_summarize_chain(
         llm, 
         chain_type="map_reduce"
     )
 
+    # Invoke Chain
     response = chain.run(chunks)
-
     return response
